d1b8b695992b5f8cedd7c778cec6a4416066aa36
groupe47 <kubra.arslan@student.vinci.be>
2021-12-21 13:12:51 +0100

    add positivityController and goodNewsTest

---
+++ tests/GoodNewsSiteTest.php
@@ -0,0 +1,12 @@
+<?php
+
+
+use PHPUnit\Framework\TestCase;
+require "vendor/autoload.php";
+require "controllers/PositivityController.php";
+
+class GoodNewsSiteTest extends TestCase
+{
+
+    private $positivityController;
+}
